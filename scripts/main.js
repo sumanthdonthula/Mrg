97b20423296484367003ec460055c84f64b1db81
sumanthdonthula <46747610+sumanthdonthula@users.noreply.github.com>
2023-03-29 17:21:22 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,96 +1,122 @@
-// Add your javascript here
-// Don't forget to add it into respective layouts where this js file is needed
-$(document).ready(function() {
-  AOS.init({
-    // uncomment below for on-scroll animations to played only once
-    // once: true
-  }); // initialize animate on scroll library
-});
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shipra weds Sumanth</title>
+    <meta name="description" content="Wonderful Wedding Template built with Bootstrap Framework for creating Wedding Website by TemplateFlip.com"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:400,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
+    <link href="css/aos.css" rel="stylesheet">
+    <link href="css/ekko-lightbox.css" rel="stylesheet">
+    <link href="styles/main.css" rel="stylesheet">
+  </head>
+  <body id="top">
+    <header></header>
+    <div class="page-content">
+      <div class="div">
+<div class="ww-home-page" id="home">
+  <div class="ww-wedding-announcement d-flex align-items-center justify-content-start">
+    <div class="container ww-announcement-container">
+      <p class="ww-couple-name ww-title" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000">Shipra weds Sumanth</p><img class="img-fluid mt-1" src="images/laurel-1.png" alt="Heart" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000"/>
+      <p class="h2 mt-5 ww-title" data-aos="zoom-in-down" data-aos-delay="300" data-aos-duration="1000" data-aos-offset="10">29<sup>th</sup> May, 2023</p>
+    </div>
+  </div>
+</div>
+<div class="ww-nav-bar sticky-top bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container"><a href="#"><svg class="heart" viewBox="0 0 32 29.6"><path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+</svg></a>
+      <h1 class="h2"><a class="pl-4 navbar-brand" href="#">Wonderful Wedding</a></h1>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ww-navbarNav" aria-controls="ww-navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse text-uppercase" id="ww-navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a class="nav-link smooth-scroll" href="#home">Home</a></li>
+          <li class="nav-item"><a class="nav-link smooth-scroll" href="#couple">Couple</a></li>
+          <li class="nav-item"><a class="nav-link smooth-scroll" href="#events">Events</a></li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
+<div class="ww-section" id="couple">
+  <div class="container">
+    <h2 class="h1 text-center pb-3 ww-title" data-aos="zoom-in-down" data-aos-duration="1000">Cheers to the Awesome Couple</h2>
+    <div class="row text-center">
+      <div class="col-md-6">
+        <div class="mt-3"><img class="img-fluid" src="images/bride.jpg" alt="Groom" width="600" height="5" data-aos="flip-left" data-aos-duration="1000"/>
+          <p>   </p>
+          
+          
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="mt-3"><img class="img-fluid" src="images/grooms.jpg" alt="Bride" width="600" height="5" data-aos="flip-right" data-aos-duration="1000"/>
+          <p>   </p>
+          
+         
+      </div>
+    </div>
+  </div>
+</div>
+<div class="ww-section bg-light" id="events">
+  <div class="container ww-wedding-event">
+    <h2 class="h1 text-center pb-3 ww-title" data-aos="zoom-in-down" data-aos-duration="1000">Wedding Events</h2>
+    <div class="row">
+      <div class="col-md-7 col-sm-12">
+        <div class="my-3">
+          <p>We cordially invite you to the wedding celebration of Shipra and Sumanth.Please attend the event and make the wedding auspicious.We will be filled with pleasure if you attend the wedding.Your blessings adds Happiness to us.</p>
+          <ul>
+          <div class="h4">Haldi Ceremony</div>
+          <li class="pt-2"><i class="text-muted far fa-calendar-alt"></i><span class="pl-2 text-muted">10:00 AM, May 28, 2023</span></li>
+        </ul>
+        <ul>
+          <div class="h4">Mehndi Party</div>
+          <li class="pt-2"><i class="text-muted far fa-calendar-alt"></i><span class="pl-2 text-muted">1:30 PM, May 28, 2023</span></li>
+        </ul>
+        <ul>
+          <div class="h4">Sangeet Event</div>
+          <li class="pt-2"><i class="text-muted far fa-calendar-alt"></i><span class="pl-2 text-muted">6:00 PM, May 28, 2023</span></li>
+        </ul>
+          <div class="h4">Wedding Party</div>
+          <ul>
+            <li class="pt-2"><i class="text-muted far fa-calendar-alt"></i><span class="pl-2 text-muted">May 29, 2023</span></li>
+          </ul>
+        <ul>
+          <li><i class="text-uppercase fas fa-map-marker-alt"></i><span class="pl-2 text-center">Chino Hills, California</span></li>
+        </ul>
+          <div class="h4">Invities</div>
+          <li>Mr.Gol Mol Papa & Mrs.Gol Mol Mumma & Golmol </li>
+          <li>Mr.Jiju Papa & Mrs.Jiju Mumma & Jiju Dii Fam </li>
+          <p>Here's the Live Link for Marriage.Click here <a href="https://www.youtube.com/watch?v=IhyHG8xSvIE&feature=youtu.be">Shipra weds Sumanth live link</a></p>
+        
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-12">
+        <div class="my-3"><img class="img-fluid" src="images/mains.jpg" alt="Wedding Party" data-aos="fade-down-right" data-aos-duration="1000"/></div>
+      </div>
+    </div>
+    
+      
+    </div>
+  </div>
+</div>
 
-// Smooth scroll for links with hashes
-$("a.smooth-scroll").click(function(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
-    location.hostname == this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $("html, body").animate(
-        {
-          scrollTop: target.offset().top
-        },
-        1000,
-        function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) {
-            // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          }
-        }
-      );
-    }
-  }
-});
+    
 
-// Photo Filter
-var activeFilter = "all";
-
-$(".ww-filter-button").on("click", function(e) {
-  // remove btn-primary from all buttons first
-  $(".ww-filter-button").removeClass("btn-primary");
-  $(".ww-filter-button").addClass("btn-outline-primary");
-
-  // add btn-primary to active button
-  var button = $(this);
-  button.removeClass("btn-outline-primary");
-  button.addClass("btn-primary");
-  filterItems(button.data("filter"));
-  e.preventDefault();
-});
-
-function filterItems(filter) {
-  if (filter === activeFilter) {
-    return;
-  }
-
-  activeFilter = filter;
-  $(".ww-gallery .card").each(function() {
-    var card = $(this);
-    var groups = card.data("groups");
-    var show = false;
-    if (filter === "all") {
-      show = true;
-    } else {
-      for (var i = 0; i < groups.length; i++) {
-        if (groups[i] === filter) {
-          show = true;
-        }
-      }
-    }
-    // hide everything first
-    card.fadeOut(400);
-    setTimeout(function() {
-      if (show && !card.is(":visible")) {
-        card.fadeIn(400);
-      }
-    }, 500);
-  });
-}
-
-// Light Box
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
+    </div>
+    <footer></footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/ekko-lightbox.min.js"></script>
+    <script src="scripts/main.js"></script>
+  </body>
+</html>
